3420bc4019bfe43fcb13ea9de4ff8cc4ce76de11
Kees van Doorn <kees.vandoorn@gmail.com>
2021-09-28 00:41:27 +0200

    added fucntions for logging in and answering questions

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open("C:\Python projects\Survey testbot\scenarios.csv", encoding='utf-8-sig
     csv_reader = csv.DictReader(infile, delimiter=";", )
     tests = list(csv_reader)
     print(tests)
-
+tests[0]
 
 driver = webdriver.Firefox('C:\Python projects\Survey testbot\geckodriver')
 
@@ -15,15 +15,31 @@ driver.get("https://q.crowdtech.com/r5r11EDq_k6lcp_I87yPWQ")
 driver.implicitly_wait(10)
 
 
-user = driver.find_element_by_xpath('//*[@id="login-email"]')
-user.send_keys(tests[0]['login'])
+def inloggen(url, login):
+    driver.get(url)
+    driver.implicitly_wait(10)
+    user = driver.find_element_by_xpath('//*[@id="login-email"]')
+    user.send_keys(login)
+    driver.find_element_by_id("login-button").click()
 
-login = driver.find_element_by_id("login-button").click()
+def vraag_antwoord(vraagnummer, antwoord):
+    #verwacht een key:value set van een dict als input
+    driver.find_element_by_id(f"{vraagnummer}_checkradio-answer-label-{antwoord}").click()
+    driver.find_element_by_id("button-next-nav").click()
+
+for schema in tests:
+    for k,v in schema.items():
+        if k == 'login':
+            inloggen("https://q.crowdtech.com/r5r11EDq_k6lcp_I87yPWQ",v)
+        if k.startswith('question'):
+            vraag_antwoord(k,v)
+
 
 antwoord = f"question-1_checkradio-answer-label-{tests[0]['V1']}"
 driver.find_element_by_id(f"question-1_checkradio-answer-label-{tests[0]['V1']}").click()
 driver.find_element_by_id("button-next-nav").click()
 driver.find_element_by_id("question-2_checkradio-answer-label-4").click()
+driver.find_element_by_id("button-next-nav").click()
 
 
 print(driver.title)
